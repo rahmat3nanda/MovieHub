bc0f96e2a70639dc0e5fe3681d76cc67a6912d3b
Rahmat Trinanda <rahmat3nanda@gmail.com>
2026-06-03 22:03:31 +0700

    Feat: Add Colors.xcassets, Icons.xcassets and Swift programmatical accessors (Colors.swift, Icons.swift)

---
+++ MovieHub/Core/Theme/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  MovieHub
+//
+//  Created by Antigravity on 03/06/26.
+//
+
+import UIKit
+
+enum Colors {
+    /// `Background` = `#111415`
+    static var background: UIColor {
+        return UIColor(named: "Background") ?? .systemBackground
+    }
+}
