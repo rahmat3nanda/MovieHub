5937157e3e7043f45441701f571e33a2327f4b67
Rahmat Trinanda <rahmat3nanda@gmail.com>
2026-06-03 21:28:57 +0700

    Initial Commit

---
+++ MovieHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieHub
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 03/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
